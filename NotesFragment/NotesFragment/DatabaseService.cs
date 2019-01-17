97915cf7ea85625e8849f308db7b4dcba1fdb660
JaanJahimees <jaanjahilo@outlook.com>
2019-01-17 08:46:25 +0200

    Added dbService

---
+++ NotesFragment/NotesFragment/DatabaseService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace NotesFragment
+{
+    public class DatabaseService
+    {
+        SQLiteConnection db;
+
+        public void CreateDatabase()
+        {
+            string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "notesDb.db3");
+            db = new SQLiteConnection(dbPath);
+            db.CreateTable<Notes>();
+        }
+
+        public void AddNote(Notes note)
+        {
+            db.Insert(note);
+        }
+
+        public void UpdateNote(Notes note)
+        {
+            db.Update(note);
+        }
+
+        public List<Notes> GetAllNotes()
+        {
+            var table = db.Table<Notes>();
+            return table.ToList();
+        }
+
+        public void DeleteNote(Notes note)
+        {
+            db.Delete(note);
+        }
+    }
+}
